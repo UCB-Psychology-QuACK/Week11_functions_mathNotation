ace1e547239d96edce6ba9435031c1ee3f18ddb4
wvoorhies <43623577+wvoorhies@users.noreply.github.com>
2020-11-10 16:22:25 -0800

    Add files via upload

---
+++ Week11_notation_starter.R
@@ -0,0 +1,32 @@
+# QuACK 11/10/2020
+# Summation 
+# Willa & Elena
+
+#### Let's see our example implemented in R. ####
+# Take the sum of numbers 1-10
+sum = 0
+for (i in 1:10){
+  sum = sum + i
+}
+
+# Write a for loop for each of the three problems on the powerpoint: 
+
+# Question 1 
+
+
+# Question 2
+
+
+# Question 3
+
+
+
+# CHALLENGE: you have to turn in several assignments over the semester. 
+# your final grade is the mean of your assignment grades. 
+# Manually calculate the mean of your grades (don't use mean())
+
+
+
+# Can you express this in mathematical notation using sigma? 
+
+
